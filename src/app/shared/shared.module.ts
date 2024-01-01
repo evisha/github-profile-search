174720b64a9402dcd1b12894789068bbf4e39f0c
Egisa <egisa.v@idobet.com>
2024-01-01 19:25:49 +0100

    add spinner when data is loading

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import {SearchBarComponent} from "./components/search-bar/search-bar.component";
 import {UserCardComponent} from "./components/user-card/user-card.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 
 
 @NgModule({
   declarations: [
     SearchBarComponent,
-    UserCardComponent
+    UserCardComponent,
+    SpinnerComponent
   ],
     exports: [
         SearchBarComponent,
-        UserCardComponent
+        UserCardComponent,
+        SpinnerComponent
     ],
   imports: [
     CommonModule,

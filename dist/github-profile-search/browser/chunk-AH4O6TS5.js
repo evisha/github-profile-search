d0e4ae3e3359ce53fe2ae1c400c293b9e7f9032a
Egisa <egisa.v@idobet.com>
2024-01-01 23:33:30 +0100

    revert last commit

--- dist/github-profile-search/browser/chunk-AH4O6TS5.js
+++
@@ -1,1 +0,0 @@
-import{m as a}from"./chunk-4SSU35H3.js";import"./chunk-LMQADJEH.js";export{a as UsersModule};
--- dist/github-profile-search/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-3TULQIGP.mjs";import"./chunk-SHCE37HS.mjs";import"./chunk-GBDGMD6X.mjs";import"./chunk-GKI55TEU.mjs";import"./chunk-H7LJY3KY.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
